4c944cf9473beb903a4fa52d939efa2c71d63049
Alexander Urrutia <matias04041994@gmail.com>
2024-05-10 17:39:29 -0400

    Home template

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "home-background": "url('/assets/home-bg.webp')",
+      },
+    },
+  },
+  plugins: ["prettier-plugin-astro"],
+};
