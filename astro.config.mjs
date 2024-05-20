d99cbd6b8b13e1f966dd31d0121dbfd596b29e3a
Alexander Urrutia <matias04041994@gmail.com>
2024-05-20 14:05:27 -0400

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), react()],
   site: "https://simplicity.cl/owine",
   base: "/owine",
   outDir: "./owine",
