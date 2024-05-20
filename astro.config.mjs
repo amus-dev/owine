92c71ec7e11d6c6f9544925f6f227e22b99cfc0f
Alexander Urrutia <matias04041994@gmail.com>
2024-05-20 14:13:56 -0400

    remove preact add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [tailwind(), preact(), react()],
+  integrations: [tailwind(), react()],
   site: "https://simplicity.cl/owine",
   base: "/owine",
   outDir: "./owine",
   build: {
-    assetsPrefix: "https://simplicity.cl/owine"
+    assetsPrefix: "https://simplicity.cl/owine",
   },
   image: {
-    service: passthroughImageService()
-  }
+    service: passthroughImageService(),
+  },
 });
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,9 @@
-import type React from "preact/compat";
-import { useState } from "preact/hooks";
 import { STATES } from "src/const/states";
 import { validateEmail } from "src/utils/formats";
 import { navigate } from "astro:transitions/client";
+import { useState } from "react";
 
-function Form() {
+const Form = () => {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
@@ -23,7 +22,6 @@ function Form() {
   };
 
   const handleValidForm = () => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (
       name === "" ||
       lastName === "" ||
@@ -41,10 +39,10 @@ function Form() {
   };
 
   return (
-    <div class="bg-[url(/owine/assets/bg-border.jpeg)] rounded-2xl w-full max-w-[630px] p-2 mt-4 animate-fade-in animate-delay-400 animate-duration-600">
-      <form class="flex flex-col justify-center items-center w-full bg-black px-3 py-4 rounded-2xl">
+    <div className="bg-[url(/owine/assets/bg-border.jpeg)] rounded-2xl w-full max-w-[630px] p-2 mt-4 animate-fade-in animate-delay-400 animate-duration-600">
+      <form className="flex flex-col justify-center items-center w-full bg-black px-3 py-4 rounded-2xl">
         <input
-          class={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[24px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && name === "" ? "!border-red-500" : "border-white"}`}
+          className={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[24px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && name === "" ? "!border-red-500" : "border-white"}`}
           type="text"
           placeholder="Name*"
           value={name}
@@ -53,7 +51,7 @@ function Form() {
           }
         />
         <input
-          class={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[24px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && lastName === "" ? "!border-red-500" : "border-white"}`}
+          className={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[24px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && lastName === "" ? "!border-red-500" : "border-white"}`}
           type="text"
           placeholder="Last name"
           value={lastName}
@@ -62,7 +60,7 @@ function Form() {
           }
         />
         <input
-          class={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[24px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && validateEmail(email) === false ? "!border-red-500" : "border-white"}`}
+          className={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[24px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && validateEmail(email) === false ? "!border-red-500" : "border-white"}`}
           type="email"
           placeholder="Email"
           value={email}
@@ -71,7 +69,7 @@ function Form() {
           }
         />
         <input
-          class={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[24px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && instagram === "" ? "!border-red-500" : "border-white"}`}
+          className={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[24px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && instagram === "" ? "!border-red-500" : "border-white"}`}
           type="text"
           placeholder="Instagram username (Public account)"
           value={instagram}
@@ -80,11 +78,12 @@ function Form() {
           }
         />
         <select
-          class={`w-full rounded-lg mb-2 text-[13px] px-[13px] text-[#C3C3C3] appearance-none form-select h-[28px] border-[2px] border-white ${validForm === true && state === "" ? "!border-red-500" : "border-white"} ${state !== "" && "!text-[#000000]"}`}
+          className={`w-full rounded-lg mb-2 text-[13px] px-[13px] text-[#C3C3C3] appearance-none form-select h-[28px] border-[2px] border-white ${validForm === true && state === "" ? "!border-red-500" : "border-white"} ${state !== "" && "!text-[#000000]"}`}
           value={state}
           onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
             setState((e.target as HTMLSelectElement)?.value);
           }}
+          defaultValue=""
         >
           {STATES.map((state, index) => (
             <option
@@ -97,21 +96,21 @@ function Form() {
           ))}
         </select>
         <textarea
-          class={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[150px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && message === "" ? "!border-red-500" : "border-white"}`}
+          className={`w-full rounded-lg mb-2 p-[13px] text-[13px] placeholder-[#C3C3C3] h-[150px] font-normal placeholder:font-normal border-[2px] border-white ${validForm === true && message === "" ? "!border-red-500" : "border-white"}`}
           placeholder="Tell us an amazing story to inspire Ami James"
           value={message}
           onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
             setMessage((e.target as HTMLTextAreaElement)?.value)
           }
         />
-        <div class="flex items-center bg-white p-[13px] rounded-lg w-full justify-between">
-          <p class="text-[13px] text-[#C3C3C3] max-w-sm">
+        <div className="flex items-center bg-white p-[13px] rounded-lg w-full justify-between">
+          <p className="text-[13px] text-[#C3C3C3] max-w-sm">
             You agree that the tatto will be done in the State of Florida during
             the month of xxxx, and the entire process will be recorded and
             dhared from Ö61 Wines* digital platforms
           </p>
           <select
-            class={`mb-2 text-[13px] w-[100px] px-2 text-[#C3C3C3] border-[#4d4d4d] border form-select-terms appearance-none ${validForm === true && terms === "" ? "!border-red-500" : "border-[#4d4d4d]"} ${terms !== "" && "!text-[#000000]"}`}
+            className={`mb-2 text-[13px] w-[100px] px-2 text-[#C3C3C3] border-[#4d4d4d] border form-select-terms appearance-none ${validForm === true && terms === "" ? "!border-red-500" : "border-[#4d4d4d]"} ${terms !== "" && "!text-[#000000]"}`}
             value={terms}
             onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
               setTerms((e.target as HTMLSelectElement)?.value)
@@ -122,22 +121,13 @@ function Form() {
             <option value="no">No</option>
           </select>
         </div>
-        <div class="flex justify-center items-center">
-          <button
-            class="g-recaptcha"
-            data-sitekey="6LclYdgpAAAAAHhWyCuOnBTOGRb8PDGYauExrQKr"
-            data-callback="onSubmit"
-            data-action="submit"
-          >
-            Submit
-          </button>
-        </div>
-        <div class="flex justify-between w-full">
-          <button class="w-[260px] h-[73px] bg-white">Captcha</button>
-          <div class="border border-white p-[4px] rounded-2xl">
+        <div className="flex justify-center items-center"></div>
+        <div className="flex justify-between w-full">
+          <button className="w-[260px] h-[73px] bg-white">Captcha</button>
+          <div className="border border-white p-[4px] rounded-2xl">
             <button
               onClick={(e) => handleOnClick(e)}
-              class="text-white bg-[url(/owine/assets/bg-border.jpeg)] font-cheddar text-[43px] rounded-xl px-10 duration-300 transition-colors hover:text-black"
+              className="text-white bg-[url(/owine/assets/bg-border.jpeg)] font-cheddar text-[43px] rounded-xl px-10 duration-300 transition-colors hover:text-black"
             >
               Enviar
             </button>
@@ -148,6 +138,6 @@ function Form() {
       </form>
     </div>
   );
-}
+};
 
 export default Form;
